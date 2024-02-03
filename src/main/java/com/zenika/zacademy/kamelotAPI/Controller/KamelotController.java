76ac025d495f7b6751618592282515a45c962fc7
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-02-03 01:17:48 +0100

    :recycle: refactoring the getQuote method to add the possibility of request params

--- src/main/java/com/zenika/zacademy/kamelotAPI/Controller/KamelotController.java
+++ src/main/java/com/zenika/zacademy/kamelotAPI/Controller/KamelotController.java
@@ -1,20 +1,27 @@
 package com.zenika.zacademy.kamelotAPI.Controller;
 
+import com.zenika.zacademy.kamelotAPI.Controller.dto.KamelotMapper;
+import com.zenika.zacademy.kamelotAPI.Controller.dto.QuoteDto;
 import com.zenika.zacademy.kamelotAPI.KamelotClient;
+import com.zenika.zacademy.kamelotAPI.Service.KamelotResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KamelotController {
     private final KamelotClient kamelotClient;
+    private final KamelotMapper kamelotMapper;
 
-
-    public KamelotController(KamelotClient kamelotClient) {
+    public KamelotController(KamelotClient kamelotClient, KamelotMapper kamelotMapper) {
         this.kamelotClient = kamelotClient;
+        this.kamelotMapper = kamelotMapper;
     }
 
     @GetMapping("/quote")
-    public void getQuote () {
-        this.kamelotClient.getRandom();
+    public QuoteDto getQuote (@RequestParam(required = false) Integer book, @RequestParam(required = false) String character) {
+        KamelotResponse kamelotResponse = kamelotClient.getRandom(book, character);
+
+        return kamelotMapper.kamelotResponseToQuoteDto(kamelotResponse);
     }
 }
--- src/main/java/com/zenika/zacademy/kamelotAPI/KamelotClient.java
+++ src/main/java/com/zenika/zacademy/kamelotAPI/KamelotClient.java
@@ -1,7 +1,13 @@
 package com.zenika.zacademy.kamelotAPI;
 
+import com.zenika.zacademy.kamelotAPI.Service.KamelotResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import javax.swing.text.html.Option;
+import java.util.Objects;
+import java.util.Optional;
 
 @Component
 public class KamelotClient {
@@ -13,7 +19,17 @@ public class KamelotClient {
         restTemplate = new RestTemplate();
     }
 
-    public void getRandom () {
-        restTemplate.getForObject(kamelotConfig.getUrl() + "/random", String.class);
+    public KamelotResponse getRandom (Integer filteredBook, String filteredCharacter) throws RestClientException {
+        String filterBookUrl = Objects.nonNull(filteredBook) ? "/livre/" + filteredBook : "";
+        String filterCharacterUrl = Objects.nonNull(filteredCharacter) ? "/personnage/" + filteredCharacter : "";
+
+        KamelotResponse response = restTemplate.getForObject(kamelotConfig.getUrl() + "/random" + filterBookUrl + filterCharacterUrl
+                , KamelotResponse.class);
+
+        if (response == null || response.getStatus() != 1) {
+            throw new RestClientException("No data found in Kaamelott API");
+        }
+
+        return response;
     }
 }
